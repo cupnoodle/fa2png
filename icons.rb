f84fb0b695ebfcbf057833ccc826ff3af1ec05a1
Axel <axelshaded@gmail.com>
2020-05-03 04:03:11 +0800

    Added size input
    Added size input

--- icons.rb
+++ icons.rb
@@ -8,14 +8,15 @@ icons_dict = YAML.load_file('icons.yml')
 
 icons_dict.each do |icon_name, icon_dict|
   icon_dict['styles'].each do |style|
-    output_arr << { label: "#{icon_name}-#{style}", value: "#{icon_name}-#{style}", id: "#{style}/#{icon_name}.svg"}
+    # output_arr << { label: "#{icon_name}-#{style}", value: "#{icon_name}-#{style}", id: "#{style}/#{icon_name}.svg"}
+    output_arr << { text: "#{icon_name}-#{style}", id: "#{style}/#{icon_name}.svg"}
     # output_dict["#{icon_name}-#{style}"] = "#{style}/#{icon_name}.svg"
   end
 end
 
-out_file = File.new("icons.json", "w")
+out_file = File.new("icons.js", "w")
 #...
-out_file.puts("#{output_arr.to_json}")
+out_file.puts("var icons = #{output_arr.to_json};")
 #...
 out_file.close
 
