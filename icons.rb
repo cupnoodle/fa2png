4ef6f6ee6ff375a2f883201fc3e26e578bcc36b1
Axel <axelshaded@gmail.com>
2020-05-02 21:39:39 +0800

    WIP

---
+++ icons.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+require 'json'
+
+
+output_arr = []
+
+icons_dict = YAML.load_file('icons.yml')
+
+icons_dict.each do |icon_name, icon_dict|
+  icon_dict['styles'].each do |style|
+    output_arr << { label: "#{icon_name}-#{style}", value: "#{icon_name}-#{style}", id: "#{style}/#{icon_name}.svg"}
+    # output_dict["#{icon_name}-#{style}"] = "#{style}/#{icon_name}.svg"
+  end
+end
+
+out_file = File.new("icons.json", "w")
+#...
+out_file.puts("#{output_arr.to_json}")
+#...
+out_file.close
+
+puts "done"
